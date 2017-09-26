747730c4b2c9477aaa3fbd7d117af8b1c66c35a4
Joe Harrison <smartthings@joeharrison.com>
2017-09-26 15:13:34 -0400

    calling service!

--- devicetypes/harrisys/ptpager.src/ptpager.groovy
+++ devicetypes/harrisys/ptpager.src/ptpager.groovy
@@ -4,7 +4,7 @@ metadata {
     }
     preferences {
         input("DeviceAddress", "string", title:"PTPager IP Address", description:"IP address of the machine running PTPager", defaultValue:"" , required: true, displayDuringSetup: true)
-        input("DevicePort", "string", title:"PTPager Port", description:"Port of the machine running PTPager", defaultValue:"5666", required: true, displayDuringSetup: true)
+        input("DevicePort", "string", title:"PTPager Port", description:"Port of the machine running PTPager", defaultValue:"5566", required: true, displayDuringSetup: true)
     }
     tiles {
         standardTile("speak", "device.speech", inactiveLabel: false, decoration: "flat") 
@@ -21,17 +21,70 @@ String getVersion() {return "0.1";}
 def speak(toSay) {
     log.debug "Executing 'speak'"
     if (!toSay?.trim()) {
-        if (ReplyOnEmpty) {
+        
             toSay = "PTPager Version ${version}"
-        }
     }
 
     if (toSay?.trim()) {
-        def command="&SPEAK="+toSay
-        return transmit(command)
+        def command="/speak"
+        
+        def query = [
+        	tosay: toSay
+        ]
+        return transmit(command, query)
     }
 }
 
-private transmit(commandString, sendToS3 = false) {
+private transmit(commandString, query) {
     log.info "Sending command "+ commandString+" to "+DeviceAddress+":"+DevicePort
+    
+    if (DeviceAddress?.trim()) {
+        def hosthex = convertIPtoHex(DeviceAddress)
+        def porthex = convertPortToHex(DevicePort)
+        device.deviceNetworkId = "$hosthex:$porthex"
+        log.info "deviceNetworkId: " + "$hosthex:$porthex"
+        
+        def result = new physicalgraph.device.HubAction(
+            method: "GET",
+            path: "/api" + commandString,
+            headers: [
+                HOST: "$DeviceAddress:$DevicePort"
+            ],
+            query: query
+        )
+		return result;
+    }
+}
+
+def parse(description) {
+	log.info "in parse method"
+    def msg = parseLanMessage(description) 
+    def headersAsString = msg.header // => headers as a string
+    def headerMap = msg.headers      // => headers as a Map
+    def body = msg.body              // => request body as a string
+    def status = msg.status          // => http status code of the response
+    def json = msg.json              // => any JSON included in response body, as a data structure of lists and maps
+    def xml = msg.xml                // => any XML included in response body, as a document tree structure
+    def data = msg.data              // => either JSON or XML in response body (whichever is specified by content-type header in response)
+    
+    log.info headersAsString
+    log.info status
+    log.info body
+}
+
+private String convertIPtoHex(ipAddress) { 
+    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02X', it.toInteger() ) }.join()
+    log.debug "IP address entered is $ipAddress and the converted hex code is $hex"
+    return hex
+
+}
+
+private String convertPortToHex(port) {
+    String hexport = port.toString().format( '%04X', port.toInteger() )
+    log.debug hexport
+    return hexport
+}
+
+void calledBackHandler(physicalgraph.device.HubResponse hubResponse) {
+    log.debug "Entered calledBackHandler()..."
 }
