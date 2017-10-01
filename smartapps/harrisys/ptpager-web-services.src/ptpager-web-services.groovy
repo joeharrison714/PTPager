c69eea99b2804be249c271e01c508e81e9361a10
Joe Harrison <smartthings@joeharrison.com>
2017-10-01 10:01:16 -0400

    web services

---
+++ smartapps/harrisys/ptpager-web-services.src/ptpager-web-services.groovy
@@ -0,0 +1,68 @@
+/**
+ *  PTPager Web Services
+ *
+ */
+definition(
+    name: "PTPager Web Services",
+    namespace: "Harrisys",
+    author: "Joe Harrison",
+    description: "PTPager web services",
+    category: "",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: [displayName: "PTPager Web Services", displayLink: "http://localhost:4567"])
+
+
+preferences {
+  section ("Allow PTPager to control these things...") {
+    input "switches", "capability.switch", multiple: true, required: true
+  }
+}
+
+mappings {
+  path("/switches") {
+    action: [
+      GET: "listSwitches"
+    ]
+  }
+  path("/switches/:command") {
+    action: [
+      PUT: "updateSwitches"
+    ]
+  }
+}
+
+// returns a list like
+// [[name: "kitchen lamp", value: "off"], [name: "bathroom", value: "on"]]
+def listSwitches() {
+
+    def resp = []
+    switches.each {
+        resp << [name: it.displayName, value: it.currentValue("switch")]
+    }
+    return resp
+}
+
+void updateSwitches() {
+    // use the built-in request object to get the command parameter
+    def command = params.command
+
+    // all switches have the comand
+    // execute the command on all switches
+    // (note we can do this on the array - the command will be invoked on every element
+    switch(command) {
+        case "on":
+            switches.on()
+            break
+        case "off":
+            switches.off()
+            break
+        default:
+            httpError(400, "$command is not a valid command for all switches specified")
+    }
+
+}
+def installed() {}
+
+def updated() {}
